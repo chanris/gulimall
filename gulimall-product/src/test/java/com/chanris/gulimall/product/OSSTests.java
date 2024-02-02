4903161d15d3d9755c40c68804a9f0eb2ed7a2a6
chenyue <2511006757@qq.com>
2024-02-02 16:54:04 +0800

    feat(gulimall/common): 项目整合alibaba oss服务

---
+++ gulimall-product/src/test/java/com/chanris/gulimall/product/OSSTests.java
@@ -0,0 +1,127 @@
+package com.chanris.gulimall.product;
+
+import com.aliyun.oss.*;
+import com.aliyun.oss.common.auth.CredentialsProviderFactory;
+import com.aliyun.oss.common.auth.EnvironmentVariableCredentialsProvider;
+import com.aliyun.oss.model.OSSObject;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+import java.io.*;
+
+/**
+ * @author chenyue7@foxmail.com
+ * @date 1/2/2024
+ * @description
+ */
+@SpringBootTest
+public class OSSTests {
+
+    @Resource
+    OSSClient ossClient;
+
+    @Test
+    void testLOSSDownloadFile() throws ClientException, com.aliyuncs.exceptions.ClientException {
+
+        // Endpoint以华东1（杭州）为例，其它Region请按实际情况填写。
+        String endpoint = "https://oss-cn-chengdu.aliyuncs.com";
+        // 从环境变量中获取访问凭证。运行本代码示例之前，请确保已设置环境变量OSS_ACCESS_KEY_ID和OSS_ACCESS_KEY_SECRET。
+        EnvironmentVariableCredentialsProvider credentialsProvider = CredentialsProviderFactory.newEnvironmentVariableCredentialsProvider();
+        // 填写Bucket名称，例如examplebucket。
+        String bucketName = "gulimall-chanris";
+        // 填写Object完整路径，例如exampledir/exampleobject.txt。Object完整路径中不能包含Bucket名称。
+        String objectName = "gulimall-chanris/14004_591f95af33.jpg";
+
+        // 创建OSSClient实例。
+        OSS ossClient = new OSSClientBuilder().build(endpoint, credentialsProvider);
+
+        try {
+            // 调用ossClient.getObject返回一个OSSObject实例，该实例包含文件内容及文件元数据。
+            OSSObject ossObject = ossClient.getObject(bucketName, objectName);
+            // 调用ossObject.getObjectContent获取文件输入流，可读取此输入流获取其内容。
+            InputStream content = ossObject.getObjectContent();
+            if (content != null) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(content));
+                while (true) {
+                    String line = reader.readLine();
+                    if (line == null) {
+                        break;
+                    }
+                    System.out.println("\n" + line);
+                }
+                // 数据读取完成后，获取的流必须关闭，否则会造成连接泄漏，导致请求无连接可用，程序无法正常工作。
+                content.close();
+            }
+        } catch (OSSException oe) {
+            System.out.println("Caught an OSSException, which means your request made it to OSS, "
+                    + "but was rejected with an error response for some reason.");
+            System.out.println("Error Message:" + oe.getErrorMessage());
+            System.out.println("Error Code:" + oe.getErrorCode());
+            System.out.println("Request ID:" + oe.getRequestId());
+            System.out.println("Host ID:" + oe.getHostId());
+        } catch (ClientException | IOException ce) {
+            System.out.println("Caught an ClientException, which means the client encountered "
+                    + "a serious internal problem while trying to communicate with OSS, "
+                    + "such as not being able to access the network.");
+            System.out.println("Error Message:" + ce.getMessage());
+        } finally {
+            if (ossClient != null) {
+                ossClient.shutdown();
+            }
+        }
+    }
+
+    @Test
+    void testOSSUploadFile() throws Exception {
+        // Endpoint以华东1（杭州）为例，其它Region请按实际情况填写。
+        String endpoint = "https://oss-cn-chengdu.aliyuncs.com";
+        // 从环境变量中获取访问凭证。运行本代码示例之前，请确保已设置环境变量OSS_ACCESS_KEY_ID和OSS_ACCESS_KEY_SECRET。
+        EnvironmentVariableCredentialsProvider credentialsProvider = CredentialsProviderFactory.newEnvironmentVariableCredentialsProvider();
+        // 填写Bucket名称，例如examplebucket。
+        String bucketName = "gulimall-chanris";
+        // 填写Object完整路径，例如exampledir/exampleobject.txt。Object完整路径中不能包含Bucket名称。
+        String objectName = "AI美女2.png";
+
+        // 创建OSSClient实例。
+        OSS ossClient = new OSSClientBuilder().build(endpoint, credentialsProvider);
+
+        try {
+            String content = "Hello OSS";
+            File file = new File("E:\\IMAGES\\12.png");
+            if (file.exists()) {
+                FileInputStream fis = new FileInputStream(file);
+//                ossClient.putObject(bucketName, objectName, new ByteArrayInputStream(content.getBytes()));
+                ossClient.putObject(bucketName, objectName, fis);
+            }
+
+        } catch (OSSException oe) {
+            System.out.println("Caught an OSSException, which means your request made it to OSS, "
+                    + "but was rejected with an error response for some reason.");
+            System.out.println("Error Message:" + oe.getErrorMessage());
+            System.out.println("Error Code:" + oe.getErrorCode());
+            System.out.println("Request ID:" + oe.getRequestId());
+            System.out.println("Host ID:" + oe.getHostId());
+        } catch (ClientException ce) {
+            System.out.println("Caught an ClientException, which means the client encountered "
+                    + "a serious internal problem while trying to communicate with OSS, "
+                    + "such as not being able to access the network.");
+            System.out.println("Error Message:" + ce.getMessage());
+        } finally {
+            if (ossClient != null) {
+                ossClient.shutdown();
+            }
+        }
+    }
+
+    @Test
+    void testOSSClient() throws Exception {
+        //上传文件流。
+        InputStream inputStream = new FileInputStream("E:\\IMAGES\\14004_e8d9efcc2a.jpg");
+        ossClient.putObject("gulimall-chanris", "小绿.jpg", inputStream);
+
+        // 关闭OSSClient。
+        ossClient.shutdown();
+        System.out.println("上传成功.");
+    }
+}
